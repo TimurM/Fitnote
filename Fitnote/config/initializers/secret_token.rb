8dd9589fefa0c208f09c7e0abd6301a592807e9c
Timur Meyster <timur.meyster@gmail.com>
2015-02-01 18:55:00 -0800

    first commit

---
+++ Fitnote/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitnote::Application.config.secret_key_base = '20500856a740a29f4bfa3317245194d775ce9c1b44554609e9b2d1ac4ee6098e6307d704cf49366d94ec0ac94bbd475b81cdfd355b05488cc563838fc68631cf'
